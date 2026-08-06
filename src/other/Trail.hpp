89b2b96d4e19f7f837b257c8d14022c89e9d9eb2
kris <foo@example.com>
2026-08-06 19:06:46 -0400

    got rid of some artifacts from Shake.hpp

--- src/other/Trail.hpp
+++ src/other/Trail.hpp
@@ -8,31 +8,17 @@ using namespace Hyprutils::Animation;
 using namespace std::chrono;
 
 class CTrail {
-  public:
-    CTrail();
-        
+  public:       
     void push(Vector2D pos);
     const std::vector<Vector2D>& get() const {return samples;}
 
     /* called when a cursor warp has happened (to avoid magnifying on warps) */
     void   warp(void);
 
-    /* force magnification regardless of speed now */
-    void force(std::optional<int> duration, std::optional<float> size);
-
   private:
         int index = 0;
         int max = 15;
-    /* tracks whether the current shake has already been announced in the ipc */
-    bool ipc = false;
-
-    bool                     started = false;
-    PHLANIMVAR<float>        zoom;
-    steady_clock::time_point end;
 
     /* ringbuffer for last samples */
     std::vector<Vector2D> samples;
-    /* we also store the distance for each sample to the last, so we do only compute this once */
-    std::vector<double>   samples_distance;
-    int                   samples_index = 0;
 };
