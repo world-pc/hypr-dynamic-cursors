e52d9b5369999a02e7dbf56a716a5d94e43cf789
kris <foo@example.com>
2026-08-08 20:37:21 -0400

    changed default values for trail config.

--- src/config/ConfigManager.cpp
+++ src/config/ConfigManager.cpp
@@ -43,7 +43,7 @@ CConfigHandler::CConfigHandler() {
     c_shakeTimeout    = conf(NS("shake:timeout"),          2000,                   "time in milliseconds the cursor will stay magnified after a shake has ended");
 
     c_trailEnabled    = conf(NS("trail:enabled"),          false,                  "enables cursor trail");
-    c_trailLength     = conf(NS("trail:length"),           15,                     "cursor trail length, in cursors.");
+    c_trailLength     = conf(NS("trail:length"),           5,                      "cursor trail length, in cursors.");
     c_trailRate       = conf(NS("trail:rate"),             5,                      "spawn a cursor every x frames.");
 
     c_highresEnabled  = conf(NS("hyprcursor:enabled"),     true,                   "enable dedicated hyprcursor support");
